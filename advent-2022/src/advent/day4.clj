aca6035be44efbc3b005a9c8d73a88ad89b1d2a3
Eric Caspary <casp@pop-os.localdomain>
2022-12-08 15:42:11 -0500

    day 4.2

--- advent-2022/src/advent/day4.clj
+++ advent-2022/src/advent/day4.clj
@@ -50,3 +50,54 @@
                    (line-seq r)))))
   ;; =>
   595)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; --- Part Two ---
+;;
+;; It seems like there is still quite a bit of duplicate work
+;; planned. Instead, the Elves would like to know the number of pairs
+;; that overlap at all.
+;;
+;; In the above example, the first two pairs (2-4,6-8 and 2-3,4-5)
+;; don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7,
+;; 6-6,4-6, and 2-6,4-8) do overlap:
+;;
+;; 5-7,7-9 overlaps in a single section, 7.
+;; 2-8,3-7 overlaps all of the sections 3 through 7.
+;; 6-6,4-6 overlaps in a single section, 6.
+;; 2-6,4-8 overlaps in sections 4, 5, and 6.
+;;
+;; So, in this example, the number of overlapping assignment pairs is 4.
+
+
+
+(def test-input-2
+  "In how many assignment pairs is there **any** overlap?"
+  ;; Same as earlier test input -- I just wanted a succinct statement of the part2 challenge
+  ["2-4,6-8"
+   "2-3,4-5"
+   "5-7,7-9"
+   "2-8,3-7" ;; 2-8 contains 3-7
+   "6-6,4-6" ;; 4-6 contains 6-6
+   "2-6,4-8"])
+
+(defn note-intersection
+  [{:keys [a b] :as pair}]
+  (let [overlap (set/intersection a b)]
+    (cond-> pair
+      (seq overlap)
+      (assoc :intersection overlap))))
+
+
+(comment
+  (let [fname input-filename]
+    (with-open [r (io/reader (io/file fname))]
+      (count (into []
+                   (comp (map (comp note-intersection parse-line))
+                         (filter :intersection))
+                   (line-seq r)))))
+  ;; => 952
+  )
